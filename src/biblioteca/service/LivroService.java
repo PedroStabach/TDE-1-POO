efe69532e24f1c1d70bb8bc8d69cd26ac0950a25
PedroStabach <pedroaugustostabach3@gmail.com>
2025-09-15 20:44:35 -0300

    Update LivroService.java

--- src/biblioteca/service/LivroService.java
+++ src/biblioteca/service/LivroService.java
@@ -1,6 +1,7 @@
 import src.biblioteca.model.Livro;
 
 package src.biblioteca.service;
+
 public class LivroService {
     public void listagemLivros(Livro[] livros) {
         for(int i = 0; i < livros.length ; i++) {
